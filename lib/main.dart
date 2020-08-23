c58b2f9d89fa4d34ea4bae5e4b74608e9a5f842a
arsenii_tishkov@mail.ru <Anastasia1>
2020-08-24 01:42:09 +0500

    Make main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app/ConnectTarif.dart';
 import 'package:flutter_app/OwnTarif.dart';
 import 'Bin.dart';
@@ -6,12 +7,18 @@ import 'Constructor.dart';
 import 'Helper.dart';
 import 'Login.dart';
 
-void main() =>runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       initialRoute: "/",
       routes: {
